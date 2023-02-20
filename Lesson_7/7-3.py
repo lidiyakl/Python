d6e722438927fefac144cefad243a249e7f338d1
«Lidiya» <lidiyakl0407@gmail.com>
2023-02-20 17:05:32 +0700

    HW Task 7-1

--- Lesson_7/7-3.py
+++ Lesson_7/7-3.py
@@ -17,8 +17,13 @@
 def same_by(condition, nums):
 	return len(set(map(condition, nums))) == 1 # длина множества равна 1
                                                # в множестве одинаковые элементы не отражаются
+
 values = [0, 2, 10, 6] 
 if same_by(lambda x: x % 2, values):
     print('same')
 else:
     print('different')
+
+# вар 2 с изменением данного в задаче кода
+# def same_by(f, values):
+# 	    return  False if 1 in set(map(f, values)) else True
